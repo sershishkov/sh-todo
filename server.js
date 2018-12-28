ddb5ea2593550d97a7e25773c5348e3f28fbb040
sershishkov <ser.shishkov@gmail.com>
2018-12-28 18:13:22 +0200

    heroku

--- server.js
+++ server.js
@@ -22,6 +22,16 @@ mongoose
  // Use Routes
 app.use('/api/task', task);
 
+//sever static assets if in production
+if ( process.env.NODE_ENV === 'production' ) {
+  //set static foder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 const port = process.env.PORT || 5000;
 
